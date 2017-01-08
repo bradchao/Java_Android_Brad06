2bdf5c659a94777d851a18f2a6ee2061cdb98a47
brad <brad@brad.tw>
2017-01-08 13:49:21 +0800

    v2

--- app/src/main/java/tw/org/iii/brad06/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad06/MainActivity.java
@@ -1,13 +1,31 @@
 package tw.org.iii.brad06;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+    private File sdroot;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String state = Environment.getExternalStorageState();
+        Log.v("brad", state);
+        sdroot = Environment.getExternalStorageDirectory();
+        Log.v("brad", sdroot.getAbsolutePath());
+
+    }
+
+
+    public void test1(View v){
+
     }
 }
