87292d561b494319d46fb5bf7a0b629b2ce6ffff
brad <brad@brad.tw>
2017-01-08 14:23:21 +0800

    v4

--- app/src/main/java/tw/org/iii/brad06/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad06/MainActivity.java
@@ -1,6 +1,11 @@
 package tw.org.iii.brad06;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,13 +27,31 @@ public class MainActivity extends AppCompatActivity {
         sdroot = Environment.getExternalStorageDirectory();
         Log.v("brad", sdroot.getAbsolutePath());
 
-        approot = new File(sdroot, "Android/data/" + getPackageName());
-        if (approot.exists()){
-            approot.mkdirs();
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else {
+            init();
         }
 
     }
 
+    private void init(){
+        approot = new File(sdroot, "Android/data/" + getPackageName());
+        if (!approot.exists()) {
+            approot.mkdirs();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        init();
+    }
 
     public void test1(View v){
 
